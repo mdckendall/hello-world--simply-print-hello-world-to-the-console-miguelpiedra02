5f172af5aef88c838d51cc3b78a2936732834e22
Miguel Piedra <124907845+miguelstrives@users.noreply.github.com>
2023-02-16 14:58:53 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,6 +1,5 @@
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!"); 
-        // My Hello World!
-    }
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
 }
